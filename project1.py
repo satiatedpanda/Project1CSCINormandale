00ae93eb6c1354c117b880fa4e189f1f07baae15
satiatedpanda <124468098+satiatedpanda@users.noreply.github.com>
2026-02-11 13:29:43 -0600

    Add files via upload

--- project1.py
+++ project1.py
@@ -22,20 +22,40 @@ person z: meek, needy lil' guy,
 
 
 """
-person1Questions = {} #food guy
-person2Questions = {} #bully
-Person3Questions = {} #meek
 
-MainScore = [1, 1, 1] #idx+1 equals person number
+#global variables
 
-
-
-def ScoreAnswer(answer):
-    result = 0
+#person1Questions = {"question":str : [["answer1": str, score: int/float], ["answer2", score2] ["answers3", score3]] <- example 
+person1 = {} #food guy
 
 
+person1keys = list(person1.keys) # this is for the quesiton indexes. This is so the questions and answers dont get mixed up
+
+#how to call
+# person1[person1keys[0]][2][1]  =  #question=idx > answer#=idx > score=1,answerstr=0
+person2 = {
+
+} #bully
+
+person3 = {
+
+} #meek
+
+
+MainScore = [] #idx+1 equals person number
+numofPeople = int(0)
+startval = 1
+
+#functions
+def ScoreAnswer(score):
+    result = -1
+
+
+
+    #MainScore[idx] = int(MainScore * 0.75)
     return(result)
     #
+
 
 
 def NextQuestion(scoremaybe, questionnumber):
@@ -45,13 +65,27 @@ def NextQuestion(scoremaybe, questionnumber):
     return()
 
 
-
 def EndingGame():
-
-    exit()
+    pass
 
 def StartGame():
-    return()
+    numpeople = int(input("number of people: 1,2 or 3\n")) #sets number of people for game
+    numofPeople = numpeople 
+    for i in range(numofPeople):
+        MainScore.append(int(startval))
+    while True: #asks for input to do tutorial or not
+        try:
+            tutorialY = input("Do Tutorial? Y/n\n")
+            corlist = ["Y", "n"]
+            if tutorialY not in corlist:
+                raise ValueError
+            if tutorialY == "Y":
+                Tutorial()
+            break
+        except ValueError:
+            print("Invalid input, please type exactly \"Y\" or \"n\"\n")
+        #block was found in stackoverflow: https://stackoverflow.com/questions/41832613/python-input-validation-how-to-limit-user-input-to-a-specific-range-of-integers
+        
 
 
 
@@ -67,17 +101,9 @@ def Updatelog():
 
 
 def MainFun():
-    while True: #asks for input to do tutorial or not
-        try:
-            tutorial = input("Do Tutorial? Y/n\n")
-            corlist = ["Y", "n"]
-            if tutorial not in corlist:
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid input, please type exactly \"Y\" or \"n\"\n")
-        #block was found in stackoverflow: https://stackoverflow.com/questions/41832613/python-input-validation-how-to-limit-user-input-to-a-specific-range-of-integers
-    
+    StartGame()
+
+
     
     
     return()
@@ -89,4 +115,5 @@ def MainFun():
 
 
 if __name__ == '__main__':
+# Write all functions in here that will be called when running the program
     MainFun()
