2940edaa6b8a31de8de536e94743195f407f23e8
poprocks-possum <clockworkmelody@gmail.com>
2026-02-17 15:47:38 -0600

    Add files via upload

---
+++ Dawn-dialogue.py
@@ -0,0 +1,15 @@
+print("""Welcome to Stay Home Moon! You’ve found yourself in a bit of a spot, 
+haven’t you? The goal of the game is pretty simple: Don’t Eat Your Friends.
+
+You will have to navigate a series of phone calls with your friends. You’ll
+be presented with a series of responses that will shape how your friend reacts 
+to the conversation. You will select your response by typing the correlating 
+number into the command line. There are three things to remember when selecting
+a response:
+
+-Your words have weight.
+-Some of your options will have a greater impact than others.
+-The ultimate goal is to not eat your friends, but try not to completely ruin 
+ your relationships.
+
+You should really pick up that phone now, Cameron’s waiting, good luck!""")
